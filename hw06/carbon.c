d0874554307701092ca77dff616843503dbe438e
Daniel Yak <danielyaa5@gmail.com>
2016-04-12 17:02:55 -0400

    fjlasdjf

--- hw06/carbon.c
+++ hw06/carbon.c
@@ -5,7 +5,7 @@ struct common *shared;//pointer to shared data structure
 void printVariables(void);
 
 int main(int argc, char const *argv[]) {
-	int semid, shmid, semw, sems;//semaphore memory id, shared memory id
+	int semid, shmid;//semaphore memory id, shared memory id
 
 	int pid = getpid();
 
@@ -28,49 +28,34 @@ int main(int argc, char const *argv[]) {
 	}
 
 	// acquire lock on mutex before accessing shared memory
-	if ((sems = semWait(semid, MUTEX)) == 0) {
-		perror("semWait");
-		exit(EXIT_FAILURE);
-	}
+	semWait(semid, MUTEX);
 
 	fflush(stdout);
-	printf("Carbon atom, pid %d, arrives at barrier\n", pid);
-	printf("Currently %d Hydrogens and %d Carbons waiting\n", shared->waiting_H, shared->waiting_C + 1);
+	printf("Hydrogen atom, pid %d, arrives at barrier\n", pid);
+	printf("Currently %d Hydrogens and %d Carbons waiting\n", shared->waiting_H + 1, shared->waiting_C);
 	fflush(stdout);
 
-	// if enough H is waiting, continue past barrier
-	if (shared->waiting_H >= 4) {
-		// release 4 H
-		for (int i=0; i < 4; i++) {
-			if ((sems = semSignal(semid, SH)) == 0) {
-				perror("semSignal");
-				exit(EXIT_FAILURE);
-			};
+	// if enough C and H is waiting, continue past barrier
+	if (shared->waiting_H >= 3 
+		&& shared->waiting_C >= 1) {
+		// release 3 H
+		for (int i=0; i < 3; i++) {
+			semSignal(semid, SH);
 		}
-		shared->waiting_H -= 4;
+		shared->waiting_H -= 3;
+		semSignal(semid, SC); // release 1 C
+		shared->waiting_C -= 1;
 
 		fflush(stdout);
 		printf("\nHello from %d, 1 CH4 molecule has xed the barrier\n\n", pid);
 		fflush(stdout);
 		// release lock on mutex
 		semSignal(semid, MUTEX);
-		if ((sems = semSignal(semid, MUTEX)) == 0) {
-			perror("semSignal");
-			exit(EXIT_FAILURE);
-		};
-		pthread_exit(NULL);
 	} else {
-		// not enough H is waiting, so wait at barrier
-		shared->waiting_C += 1;
+		// not enough C or H is waiting, so wait at barrier
+		shared->waiting_H += 1;
 		// relaese lock on mutex
-		if ((sems = semSignal(semid, MUTEX)) == 0) {
-			perror("semSignal");
-			exit(EXIT_FAILURE);
-		};
-
-		if ((sems = semWait(semid, SC)) == 0) {
-			perror("semWait");
-			exit(EXIT_FAILURE);
-		}
+		semSignal(semid, MUTEX);
+		semWait(semid, SH);
 	}
 }
